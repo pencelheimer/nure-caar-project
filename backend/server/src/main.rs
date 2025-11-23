593c27f2d3a87b53c096bdb9c75b5db5d0b9892e
pencelheimer <pencelheimer@proton.me>
2025-11-23 22:59:51 +0200

    feat(api, db): added migrations to the api

--- backend/server/src/main.rs
+++ backend/server/src/main.rs
@@ -2,6 +2,8 @@ use std::{error::Error, result::Result};
 
 use axum::{Router, routing::get};
 use server::config::Config;
+use sea_orm::Database;
+use migrations::{ MigratorTrait, Migrator };
 use tokio::net::TcpListener;
 
 #[tokio::main]
@@ -10,6 +12,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
     dotenvy::dotenv().unwrap_or_default();
 
     let config = Config::new();
+    let db = Database::connect(config.db_connection_str()).await?;
+
+    Migrator::fresh(&db).await?;
 
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
